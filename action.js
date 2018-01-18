b8f39f761b389fbf7b81205ec172066bdc7c9d65
Jerliyah <jerliyahcraig@gmail.com>
2018-01-17 18:19:29 -0600

    css tranform and transition

--- action.js
+++ action.js
@@ -1,14 +1,20 @@
+// DOM Grab
+const slides = Array.from( [...document.querySelectorAll('img.slide-in')] );
+
+
+// Functions
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
     };
-  }
+}
