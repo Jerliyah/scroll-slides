26730804e32a80e24aec46a50080120f30909d1f
Jerliyah <jerliyahcraig@gmail.com>
2018-01-17 19:12:25 -0600

    done (not very happy with it though)

--- action.js
+++ action.js
@@ -18,3 +18,27 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 }
+
+function peek_slides() {
+    let scroll_bottom = window.scrollY + window.innerHeight;
+    let center_screen = window.innerHeight / 2
+
+    slides.forEach( (slide) => {
+        // Ugh, I don't quie understand the math
+        let scroll_in_at = scroll_bottom - (slide.height / 2)
+        let slide_bottom = slide.offsetTop + slide.height
+        
+        let is_half_shown = scroll_in_at > slide.offsetTop;
+        let is_not_scrolled_past = window.scrollY < slide_bottom
+
+        if( is_half_shown && is_not_scrolled_past ) {
+            slide.classList.add('active')
+        }
+        else {
+            slide.classList.remove('active')
+        }
+    })
+}
+
+// Events
+window.addEventListener('scroll', debounce(peek_slides))
